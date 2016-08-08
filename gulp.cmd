1e57de6b574ed8bfdb1ef49990acbbd812b3b1ef
Marcin <marcin37@yahoo.com>
2016-08-08 07:55:12 -0700

    Added gulp task, some cleanup.

---
+++ gulp.cmd
@@ -0,0 +1,1 @@
+node_modules\.bin\gulp.cmd %1
---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp'),
+    gulpUtil = require('gulp-util'),
+    gulpConcat = require('gulp-concat'),
+    gulpUglify = require('gulp-uglify'),
+    gulpSourcemaps = require('gulp-sourcemaps'),
+    gulpStripDebug = require('gulp-strip-debug')
+    gulpExpect = require('gulp-expect-file');
+
+gulp.task('default', ['js']);
+
+/**
+ * Minify JS
+ */
+gulp.task('js', function() {
+    
+	var sources = ['src/**/*.js'];
+
+	return gulp.src(sources)	
+                .pipe(gulpExpect(sources))
+                .pipe(gulpSourcemaps.init())                
+                .pipe(gulpConcat('jquery.nozzle.min.js'))
+                .pipe(gulpStripDebug())
+                .pipe(gulpUglify({
+                        mangle: true,
+                        source_map: 'source_map'
+                }).on('error', gulpUtil.log))				
+                .pipe(gulpSourcemaps.write('./'))
+                .pipe(gulp.dest('./dist'));    
+    
+});
+
